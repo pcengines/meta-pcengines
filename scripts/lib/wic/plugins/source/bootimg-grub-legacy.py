90232e028a0b0d1d94dbce0ea6d4f6f38c1ff199
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2020-10-15 09:48:45 +0200

    bootimg-grub-legacy.py: use libdir instead of hardcoding lib64
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- scripts/lib/wic/plugins/source/bootimg-grub-legacy.py
+++ scripts/lib/wic/plugins/source/bootimg-grub-legacy.py
@@ -178,7 +178,7 @@ class BootimgGrubLegacyPlugin(SourcePlugin):
         exec_cmd(install_cmd)
 
         # Copying grub modules
-        grub_dir_native = get_bitbake_var("IMAGE_ROOTFS") + "/usr/lib64/grub"
+        grub_dir_native = get_bitbake_var("IMAGE_ROOTFS") + get_bitbake_var("libdir") + "/grub"
         shutil.copytree("%s/i386-pc" % (grub_dir_native),
                         "%s/grub/i386-pc" % hdddir)
 
